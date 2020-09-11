2b34d25e91ef1becebb14d6b25812cfb71e0380e
Alexander Peter <alexander.peter@gmail.com>
2020-09-10 16:34:16 -1000

    tupe message

---
+++ main.php
@@ -0,0 +1,6 @@
+<?php
+$url='https://maker.ifttt.com/trigger/mask_detected/with/key/b2AiYiZeiIrrzjeP2IHw1_';
+$contents = file_get_contents($url ); // please do some sanitation here...
+// i'm just showing an example.
+echo $contents;
+?>
